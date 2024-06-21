569a2156def4289e3ab22979d2df633016fe3542
Alfred Odey <alfredmodey@gmail.com>
2024-06-22 00:14:32 +0100

    Question 2

--- Program.cs
+++ Program.cs
@@ -8,8 +8,26 @@ namespace ResizingListCapacity
 {
     internal class Program
     {
+        /*
+        Question 2: Resizing a List Capacity
+        Create a list of integers and set an
+        initial capacity of 100.
+        Add 10 integers to the list.
+        Print the capacity and count of the list.
+        */
         static void Main(string[] args)
         {
+            List<int> listOfIntegers = new List<int>(100);
+
+            // Using a For Loop to add 10 integers to the list.	
+            for (int x = 0; x < 10; x++)
+            {
+                listOfIntegers.Add(x);
+            }
+
+            Console.WriteLine($"Capacity of list: {listOfIntegers.Capacity}");
+
+            Console.WriteLine($"Count of list: {listOfIntegers.Count}");
         }
     }
 }
